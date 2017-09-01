4b369041e0f35032f66f1d47b1e08dba096b0c4b
Fer <castro.fj@gmail.com>
2017-09-01 01:16:57 -0400

    break time lesson 1

---
+++ Python/break_time.py
@@ -0,0 +1,7 @@
+import time
+import webbrowser
+count = 1
+while(count <=3):
+    time.sleep(5)
+    webbrowser.open("fercastro.com/learning")
+    count = count+1
---
+++ Python/test.py
@@ -0,0 +1,5 @@
+num = 1
+while(num <=5):
+    print("hello"), num
+    num = num+1
+    
