a3e02dd1ffa73e5597965c65d23ca2a67ed0be14
Fer <castro.fj@gmail.com>
2017-09-02 19:02:46 -0400

    change name program

--- Python/break_time.py
+++ Python/break_time.py
@@ -2,6 +2,8 @@ import time
 import webbrowser
 count = 1
 while(count <=3):
+    print(time.ctime())
+    print(time.gmtime())
     time.sleep(5)
     webbrowser.open("fercastro.com/learning")
     count = count+1
